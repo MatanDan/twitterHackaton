2129701c5ee346720089222ab860c629c64edbea
hackaton <hackaton@gmail.com>
2019-05-27 12:08:15 +0300

    feat(senders): create senders config file

---
+++ config/senders.js
@@ -0,0 +1,28 @@
+const Twitter = require('twitter');
+
+module.exports = {
+    "center": new Twitter({
+        consumer_key: 'ZodQQ7OaRigtbhZfW9EPvLD4t',
+        consumer_secret: 'i5JESEyO1tBYXugE1liP9C6yzxzDoSqdUZztsMvawubQTxNXiC',
+        access_token_key: '714897086525923328-7wwWscieeB3IYvn2v5xy44qkDx64xzH',
+        access_token_secret: 'bWwCvpZBzOvGKHdQgeaYPrHcTv4y2L6ab54qgxHH5USmA'
+      }),
+    "north": new Twitter({
+        consumer_key: 'ZodQQ7OaRigtbhZfW9EPvLD4t',
+        consumer_secret: 'i5JESEyO1tBYXugE1liP9C6yzxzDoSqdUZztsMvawubQTxNXiC',
+        access_token_key: '714897086525923328-7wwWscieeB3IYvn2v5xy44qkDx64xzH',
+        access_token_secret: 'bWwCvpZBzOvGKHdQgeaYPrHcTv4y2L6ab54qgxHH5USmA'
+      }),
+    "south": new Twitter({
+        consumer_key: 'ZodQQ7OaRigtbhZfW9EPvLD4t',
+        consumer_secret: 'i5JESEyO1tBYXugE1liP9C6yzxzDoSqdUZztsMvawubQTxNXiC',
+        access_token_key: '714897086525923328-7wwWscieeB3IYvn2v5xy44qkDx64xzH',
+        access_token_secret: 'bWwCvpZBzOvGKHdQgeaYPrHcTv4y2L6ab54qgxHH5USmA'
+      }),
+    "jerusalem": new Twitter({
+        consumer_key: 'ZodQQ7OaRigtbhZfW9EPvLD4t',
+        consumer_secret: 'i5JESEyO1tBYXugE1liP9C6yzxzDoSqdUZztsMvawubQTxNXiC',
+        access_token_key: '714897086525923328-7wwWscieeB3IYvn2v5xy44qkDx64xzH',
+        access_token_secret: 'bWwCvpZBzOvGKHdQgeaYPrHcTv4y2L6ab54qgxHH5USmA'
+      })
+}
