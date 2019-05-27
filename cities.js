e5db2985b4df97d246dd6912935f6f3131f20ffa
hackaton <hackaton@gmail.com>
2019-05-27 11:40:48 +0300

    feat(cities): added cities file

---
+++ cities.js
@@ -0,0 +1,82 @@
+module.exports = [
+    {
+        "id": 1036,
+        "name": "עומר",
+        "region": "south"
+    },
+    {
+        "id": 1048,
+        "name": "באר שבע צפון",
+        "region": "south"
+    },
+    {
+        "id": 1049,
+        "name": "בער שבע מרכז",
+        "region": "south"
+    },
+    {
+        "id": 1050,
+        "name": "באר שבע דרום",
+        "region": "south"
+    },
+    {
+        "id": 1051,
+        "name": "באר שבע מערב",
+        "region": "south"
+    },
+    {
+        "id": 1070,
+        "name": "להבים",
+        "region": "south"
+    },
+    {
+        "id": 1056,
+        "name": "שדרות",
+        "region": "south"
+    },
+    {
+        "id": 1001,
+        "name": "תל אביב דרום",
+        "region": "center"
+    },
+    {
+        "id": 1002,
+        "name": "תל אביב עבר הירקון",
+        "region": "center"
+    },
+    {
+        "id": 1003,
+        "name": "תל אביב מרכז",
+        "region": "center"
+    },
+    {
+        "id": 1004,
+        "name": "תל אביב מזרח",
+        "region": "center"
+    },
+    {
+        "id": 1005,
+        "name": "גבעתיים",
+        "region": "center"
+    },
+    {
+        "id": 1006,
+        "name": "רמת גן",
+        "region": "center"
+    },
+    {
+        "id": 1007,
+        "name": "בני ברק",
+        "region": "center"
+    },
+    {
+        "id": 1500,
+        "name": "ירושלים",
+        "region": "jerusalem"
+    },
+    {
+        "id": 1600,
+        "name": "חיפה",
+        "region": "north"
+    },
+]
